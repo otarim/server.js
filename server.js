8498dc29e76f7fc3d3183bcf37987f9f9d948f3b
otarim <otarim.com@gmail.com>
2014-12-31 22:47:57 +0800

    v2

--- server.js
+++ server.js
@@ -1,12 +1,71 @@
+var conf = JSON.stringify(fs.readFileSync('./server.conf'))
 var http = require('http'),
 	url = require('url'),
 	path = require('path'),
-	colors = require('colors'),
+	colors = require('./colors'),
+	dns = require('dns'),
 	fs = require('fs'),
-	port = 8080;
+	zlib = require('zlib'), // node has its own zlib package
+	port = conf.port,
+	cache = {} // cache[absPath] = data;
+
+var ignoreExtname = conf.ignoreExtname
+var DIR = path.join(__dirname,conf.root)
+var styleText = conf.style
+var request = function(req,res){
+	var request = http.request(req.reqConfig,function(response){
+		var chunk = ''
+		response.on('data',function(d){
+			chunk += d
+		}).on('end',function(){
+			res.writeHead(response.statusCode, response.headers)
+			res.end(chunk)
+		})
+	})
+	req.reqConfig.method === 'post' && request.write(req.postData)
+	return request.end()
+}
+var domainMap = conf.domainMap
+
 http.createServer(function(req, res) {
+	var host = req.headers.host,
+		mapDir = url.parse(req.url).pathname,
+		method = req.method.toLowerCase(),
+		query = url.parse(req.url).query || ''
+	if(host in domainMap){
+		var customDomain = domainMap[host]
+		var reqConfig = {
+			hostname: '127.0.0.1',
+			method: method,
+			headers: req.headers
+		}
+		for(var i in customDomain){
+			if(customDomain.hasOwnProperty(i)){
+				reqConfig[i] = customDomain[i]
+			}
+		}
+		if(method === 'post'){
+			reqConfig['path'] = mapDir
+			var chunk = [],len = 0
+			return req.on('data',function(d){
+				chunk.push(d)
+				len += d.length
+			}).on('end',function(){
+				chunk = Buffer.concat(chunk,len).toString()
+				req.headers['Content-length'] = chunk.length
+				req.reqConfig = reqConfig
+				req.postData = chunk
+				return request(req,res)
+			})
+		}else{
+			reqConfig['path'] = mapDir + (query ? '?' + query : '')
+			req.reqConfig = reqConfig
+			return request(req,res)
+		}
+	}
+
 	// 针对中文路径以及空格进行编码
-	var pathname = __dirname + decodeURI(url.parse(req.url).pathname),
+	var pathname = path.join(DIR,decodeURI(url.parse(req.url).pathname)),
 		consoles = {
 			'ip': req.connection.remoteAddress,
 			'url': req.headers.host + req.url
@@ -20,18 +79,25 @@ http.createServer(function(req, res) {
 	if (path.extname(pathname) === '') {
 		pathname += '/'
 	}
-	console.log(pathname);
 	fs.exists(pathname, function(exists) {
 		// req.header.host 
 		// req.url 不带协议以及host
 		if (exists) {
 			fs.stat(pathname, function(err, stat) {
 				if (stat.isFile()) {
+					/**
+					 * add gzip support
+					 * 
+					 */
 					switch (path.extname(pathname)) {
 						case '.html':
 						case '.htm':
+						case '.jsp':
 							res.writeHead(200, {
 								'Content-type': 'text/html',
+								// 'Access-Control-Allow-Origin': req.headers['origin'],
+								// 'Access-Control-Allow-Credentials': true,
+								// 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
 							});
 							break;
 						case '.xml':
@@ -46,8 +112,15 @@ http.createServer(function(req, res) {
 							break;
 						case '.js':
 							res.writeHead(200, {
-								'Content-type': 'text/javascript'
+								'Content-type': 'text/javascript',
+								'Content-Encoding': 'gzip', //necessary
 							});
+							if(req.headers['accept-encoding'] && req.headers['accept-encoding'].indexOf('gzip') !== -1){
+								zlib.gzip(fs.readFileSync(pathname),function(err,data){
+								    res.end(data);
+								});
+								return;
+							}
 							break;
 						case '.json':
 							res.writeHead(200, {
@@ -94,12 +167,18 @@ http.createServer(function(req, res) {
 								'Content-type': 'audio/mpeg',
 								'Content-length': stat.size
 							})
-							var mediaStream = fs.createReadStream(pathname);
-							mediaStream.pipe(res);
+							// var mediaStream = fs.createReadStream(pathname);
+							// mediaStream.pipe(res);
 							break;
 						case '.swf':
 							res.writeHead(200, {
 								'Content-type': 'application/x-shockwave-flash',
+								'Content-length': stat.size
+							});
+							break;
+						case '.ico':
+							res.writeHead(200, {
+								'Content-type': 'image/x-icon',
 								'Content-length': stat.size
 							});
 							break;
@@ -109,42 +188,44 @@ http.createServer(function(req, res) {
 								'Content-length': stat.size
 							})
 					}
-					fs.readFile(pathname, function(err, data) {
-						res.end(data);
-					})
+					fs.createReadStream(pathname).pipe(res)
 				} else if (stat.isDirectory()) {
 					// 构建索引
 					var _pathname = decodeURI(req.url);
 					if (_pathname.charAt(_pathname.length - 1) !== '/') {
-						_pathname += '/';
+						_pathname += '/'
 					}
 					fs.readdir(path.normalize(pathname), function(err, files) {
-						var tmp = req.headers.host + _pathname.replace(/(.+)\b\/?/g, '$1');
-						tmp = tmp.split('/');
-						tmp.pop();
+						var tmp = req.headers.host + _pathname.replace(/(.+)\b\/?/g, '$1')
+						tmp = tmp.split('/')
+						tmp.pop()
 						var rootDir = tmp.join('/') + '/',
-							ret = ['<h1>Index of ' + _pathname + '</h1><ul><li><a href="http://' + rootDir + '">../</a></li>'];
+							ret = ['<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no"/><meta charset="UTF-8"><title>'+_pathname+'</title><style>'+styleText+'</style></head><body><h1>Index of ' + _pathname + '</h1><ul><li><a href="http://' + rootDir + '">../</a></li>']
 						files.forEach(function(file) {
-							ret.push('<li><a href="http://' + req.headers.host + _pathname + file + '">' + file + '</a></li>')
+							if(ignoreExtname.some(function(ext){
+								return file === ext
+							})){
+								return
+							}
+							ret.push('<li '+(fs.statSync(path.join(pathname,file)).isDirectory() ? 'class="dir"' : '')+'><a href="http://' + req.headers.host + _pathname + file + '">' + file + '</a></li>')
 						})
-						ret.push('</ul>');
+						ret.push('<li></li></ul></body></html>')
 						res.writeHead(200, {
-							'Content-Type': 'text/html'
+							'Content-Type': 'text/html; charset=utf-8'
 						})
-						res.end(ret.join(''));
+						res.end(ret.join(''))
 					})
 				}
-
 			})
 		} else {
-			is404 = true;
+			is404 = true
 			res.writeHead(404, {
-				'Content-type': 'text/html',
+				'Content-type': 'text/html; charset=utf-8',
 				'Server': 'node.js!!!!!!'
-			});
-			res.end('<h1>goddamn 404</h1>')
+			})
+			res.end('<!DOCTYPE html><html><head><meta charset="UTF-8"><title>404!</title><style>'+styleText+'</style></head><body><p style="font-family: monaco;font-size: 3rem;text-align: center;position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);-webkit-transform: -webkit-translate(-50%,-50%);">U GOT A GODDAMN 404 PAGE ~ <sub style="font-size: 50%;">▔fﾟﾟ▔</sub></p></body></html>')
 		}
 		console.log('guest:'.red + consoles.ip.yellow.bold + ' ' + (is404 ? consoles.url.redBG : consoles.url.green))
-	})
-}).listen(port);
-console.log('servering...');
+	})	
+}).listen(port)
+console.log('servering...')
